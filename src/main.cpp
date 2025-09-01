163c751cde0b4bbe174ecac9790270b1f12af4b1
Yutong Deng <ydeng@c2r3p8.hive.fi>
2025-09-01 15:56:25 +0300

    submit version

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,12 @@ int main(int argc, char* argv[])
     }
     std::string filename = argv[1];
 
-    if (parser.parseFile(filename) == 1) {
-        std::cout << "File parsed successfully!" << std::endl;
-		parser.printSummary();
-		SoundMaking test(parser);
-		//test.printTrack();
-		test.makeSound();
-    	std::cout << "Done." << std::endl;
-        return (0);
-    }
-    return (1);
+    if (parser.parseFile(filename) == 0)
+	{
+		std::cerr << "Parsing failed\n";
+		return (1);
+	}
+	SoundMaking test(parser);
+	test.makeSound();
+	return (0);
 }
